80394b8053742537b9d3a5f516e230b355655880
Ciao <yasuhiro.iwai@aicurion.com>
2024-12-06 18:37:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -222,8 +222,6 @@ languageButtons.forEach(button => {
         updateButtonTexts();
         updateLanguageIcon(currentLanguage);
     });});
-updateLanguageIcon(currentLanguage);
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('three-canvas') });
